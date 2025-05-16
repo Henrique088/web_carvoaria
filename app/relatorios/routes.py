83422b067cdf1db23c32ad0944fcaa00d76ac86e
Henrique088 <henriqueribeiro88bb@gmail.com>
2025-05-16 10:08:40 -0300

    relatorios concluidos

--- app/relatorios/routes.py
+++ app/relatorios/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from app.models import ProducaoDiaria
 from datetime import datetime
+from .mock_dados import *
 
 
 relatorios = Blueprint('relatorios', __name__,template_folder='templates', static_folder='static')
@@ -40,16 +41,9 @@ def gerar_relatorio():
     #     ],
     #     "producao_toneladas": [2, 1, 2, 0, 3, 4, 2, 2, 2, 1, 3, 4, 3, 2, 2, 0, 1, 2, 2, 3, 2, 1, 3, 2, 1, 0, 2, 1, 0, 3, 2],
     # }
-    producao = [1,2,0,0,2,3,1]*4
-    fornos = [
-        {"numero": 1, "nome": "Forno 1", "dias": ['L', 'C', 'C', 'R', 'R', 'D', 'V'] * 4, "producao": [1,2,0,0,2,3,1]*4},
-        {"numero": 2,"nome": "Forno 2", "dias": ['V', 'L', 'C', 'C', 'R', 'D', 'V'] * 4, "producao": [1,2,0,0,2,3,1]*4},
-        {"numero": 3,"nome": "Forno 3", "dias": ['D', 'V', 'L', 'C', 'M', 'R', 'V'] * 4, "producao": [1,2,0,0,2,3,1]*4},
-        {"numero": 4,"nome": "Forno 4", "dias": ['C', 'C', 'R', 'D', 'V', 'L', 'L'] * 4, "producao": [1,2,0,0,2,3,1]*4},
-        {"numero": 5,"nome": "Forno 5", "dias": ['R', 'D', 'V', 'L', 'C', 'C', 'R'] * 4, "producao": [1,2,0,0,2,3,1]*4},
-    ]
     
-    return render_template("relatorios/relatorios.html", fornos = fornos, dias_semana=dias_semana, dias_mes=dias_mes, producao=producao)
+    
+    return render_template("relatorios/relatorios.html", fornos = fornos, dias_semana=dias_semana, dias_mes=dias_mes, producao=producao,producao_re=producao_re,producao_1=producao_1, producao_2=producao_2, lenha_1=lenha_1, lenha_2=lenha_2, planejado_l=planejado_l, planejado_c=planejado_c, planejado_r=planejado_r, planejado_d=planejado_d, planejado_v=planejado_v, planejado_m=planejado_m, planejado=planejado, realizado= realizado, realizado_l=realizado_l, realizado_c=realizado_c, realizado_r=realizado_r, realizado_d=realizado_d, realizado_v=realizado_v, realizado_m=realizado_m)
 
 
 @relatorios.route('/lista')
